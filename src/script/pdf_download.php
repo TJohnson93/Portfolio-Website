90d85e5d52f09be703235bc1bdc2fe44f46b536f
Todd Johnson <toddajohnson93@gmail.com>
2016-12-07 21:41:43 +1000

    Updated resume filename

--- src/script/pdf_download.php
+++ src/script/pdf_download.php
@@ -1,6 +1,6 @@
 <?php
 
-	$file="../doc/resume_20150728.pdf"; //file location
+	$file="../doc/resume.pdf"; //file location
 
 		header('Content-Type: application/octet-stream');
 		header('Content-Disposition: attachment; filename="Todd_Johnson_Resume.pdf"');
